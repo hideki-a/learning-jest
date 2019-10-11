f525eb5cc7a2ef9304e4becc05e43485a21ab557
Hideki Abe <hideki.abe.0630@gmail.com>
2019-10-12 06:31:19 +0900

    review of repeated event test

--- test/repeat_events.test.js
+++ test/repeat_events.test.js
@@ -5,7 +5,8 @@ const EventList = require('../src/eventlist')
 describe('期間指定イベントの処理', () => {
     let instance;
     beforeEach(() => {
-        instance = new EventList();
+        instance = new EventList()
+        instance.currentDate = new Date(2019, 10 - 1, 5)  // set 2019-10-05
     })
     it('今日以降のイベントが配列に追加されていること', () => {
         instance._pushRepeatEvent({
@@ -17,16 +18,46 @@ describe('期間指定イベントの処理', () => {
             "repeatDayOfWeek": [],
             "priority": 0
         })
-        expect((() => {
-            let result = true
-            const today = new Date()
-            const todayInfoArray = [today.getFullYear(), today.getMonth(), today.getDate()]
-            instance.eventsForDisplay.forEach((event) => {
-                if (moment(event.startDate).isBefore(todayInfoArray)) {
-                    result = false
-                }
-            })
-            return result
-        })()).toBeTruthy()
+        expect(instance.eventsForDisplay.length).toBe(6)
+        expect(instance.priorityEventsForDisplay.length).toBe(0)
+    })
+    it('今日以降のイベント（常に上に表示）が配列に追加されていること', () => {
+        instance._pushRepeatEvent({
+            "id": 1,
+            "title": "お米フェア",
+            "regularlyClosed": false,
+            "startDate": "2019-10-01",
+            "endDate": "2019-10-10",
+            "repeatDayOfWeek": [],
+            "priority": 1
+        })
+        expect(instance.eventsForDisplay.length).toBe(0)
+        expect(instance.priorityEventsForDisplay.length).toBe(6)
+    })
+    it('今日以降の曜日指定イベントが配列に追加されていること', () => {
+        instance._pushRepeatDayOfTheWeekEvent({
+            "id": 1,
+            "title": "お米フェア",
+            "regularlyClosed": false,
+            "startDate": "2019-10-01",
+            "endDate": "2019-10-10",
+            "repeatDayOfWeek": [0, 2, 4],
+            "priority": 0
+        })
+        expect(instance.eventsForDisplay.length).toBe(3)
+        expect(instance.priorityEventsForDisplay.length).toBe(0)
+    })
+    it('今日以降の曜日指定イベント（常に上に表示）が配列に追加されていること', () => {
+        instance._pushRepeatDayOfTheWeekEvent({
+            "id": 1,
+            "title": "お米フェア",
+            "regularlyClosed": false,
+            "startDate": "2019-10-01",
+            "endDate": "2019-10-10",
+            "repeatDayOfWeek": [0, 2, 4],
+            "priority": 1
+        })
+        expect(instance.eventsForDisplay.length).toBe(0)
+        expect(instance.priorityEventsForDisplay.length).toBe(3)
     })
 })
